6d17b7d2a65911372608b05c2006d533b5c2642e
Padmajase <padmajapawar7@gmail.com>
2022-04-22 23:01:41 -0700

    Added : AddressBook main program UC5

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,49 +3,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 class AddressBookMain {
-	public static void main(String args[]) {
+	public static void main(String args[])
+	{
 		System.out.println("	 Welcome To AddressBook !");
 		System.out.println("	**************************");
 		
-		Contacts.deleteContact();
+		Contacts people1 = new Contacts();
+		people1.addContact();
 	}
 }
 
-class Contacts {
-	static void deleteContact()
+// Adding multiple peoples to address book
+class Contacts
+{
+	public void addContact()
 	{
 		ArrayList<String> firstContact = new ArrayList<String>(7);
 		Scanner sc = new Scanner(System.in);
-		
+		int count = 0;
 		System.out.println(" ");		
 		System.out.println("Enter the contact details  : ");
 		System.out.println("name address city state zip phoneNo email  : ");
 		
-		/**adding contact using for loop and Arraylist **/
-
-		for(int i =0; i<7; i++)
+		for(int i =0; i < firstContact.size(); i++)
 		{
 			String s = sc.nextLine();
 			firstContact.add(s);
 		}
+		count++;
 		System.out.println(" -------------------------------------------");
-		System.out.println("contact details added :");
-		System.out.println(firstContact );
-	
-		System.out.println(" ");
-
-		/**deleting the added contact using Arraylist method **/
-
-		System.out.println("Enter the name of contact that have to delete  : ");
-		String nameToDelete = sc.nextLine();
-	
-		if(nameToDelete.equals(firstContact.get(0)))
-		{
-			firstContact.removeAll(firstContact);
-		}
-		System.out.println(" ");
-		System.out.println("contact deleted successfuly ");
-		System.out.println(firstContact );
+		System.out.println("contact" + count+ " details added :");
+		System.out.println("contact" + count + " is "+ firstContact );
 		
 	}
 }
