ac37184a28635bb1dec9d86e916bbfb8dd9cfa16
Padmajase <padmajapawar7@gmail.com>
2022-04-22 23:03:07 -0700

    Added : AddressBook main program UC6

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -8,13 +8,37 @@ class AddressBookMain {
 		System.out.println("	 Welcome To AddressBook !");
 		System.out.println("	**************************");
 		
-		Contacts people1 = new Contacts();
+		AddressBookOne people1 = new AddressBookOne();
+		people1.addContact();
+		AddressBookTwo people1 = new AddressBookTwo();
 		people1.addContact();
 	}
 }
 
-// Adding multiple peoples to address book
-class Contacts
+// Adding multiple  address book to the system
+class AddressBookOne {
+	public void addContact()
+	{
+		ArrayList<String> firstContact = new ArrayList<String>(7);
+		Scanner sc = new Scanner(System.in);
+		int count = 0;
+		System.out.println(" ");		
+		System.out.println("Enter the contact details  : ");
+		System.out.println("name address city state zip phoneNo email  : ");
+		
+		for(int i =0; i < firstContact.size(); i++)
+		{
+			String s = sc.nextLine();
+			firstContact.add(s);
+		}
+		count++;
+		System.out.println(" -------------------------------------------");
+		System.out.println("contact" + count+ " details added :");
+		System.out.println("contact" + count + " is "+ firstContact );
+	}
+}
+
+class AddressBookTwo
 {
 	public void addContact()
 	{
